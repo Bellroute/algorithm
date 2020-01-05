75b1b083d209a58665f501ccc734a819636fd3fe
Bellroute <kjkun7631@gmail.com>
2020-01-06 01:08:35 +0900

    refator: baekjoon1541

--- src/beckjoon/BaekJoon1541.java
+++ src/beckjoon/BaekJoon1541.java
@@ -1,49 +1,30 @@
 package beckjoon;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class BaekJoon1541 {
     private static Scanner scanner = new Scanner(System.in);
-    private static List<String> splits = new ArrayList<>();
 
     public static void main(String[] args) {
         String input = scanner.nextLine();
-        int sum = 0;
+        int total = 0;
 
-        for (int i = 0; i < input.split("-").length; i++) {
-            splits.add(input.split("-")[i]);
-        }
+        String[] splitStrByMinus = input.split("-");
 
-        if (splits.get(0).equals("")) {
-            splits.set(1, "-" + splits.get(1));
-        }
+        for (int i = 0; i < splitStrByMinus.length; i++) {
 
-        for (int i = 0; i < splits.size(); i++) {
-            if (splits.get(i).equals("")) {
-                continue;
-            }
-
-            if (splits.get(i).contains("+")) {
-                if (i == 0) {
-                    sum += adder(splits.get(i));
-                } else {
-                    sum -= adder(splits.get(i));
-                }
+            if (i == 0) {
+                total += calculateAddExpression(splitStrByMinus[i]);
             } else {
-                if (i == 0) {
-                    sum += Integer.parseInt(splits.get(i));
-                } else {
-                    sum -= Integer.parseInt(splits.get(i));
-                }
+
+                total -= calculateAddExpression(splitStrByMinus[i]);
             }
         }
 
-        System.out.println(sum);
+        System.out.println(total);
     }
 
-    private static int adder(String str) {
+    private static int calculateAddExpression(String str) {
         int result = 0;
 
         for (int i = 0; i < str.split("\\+").length; i++) {
