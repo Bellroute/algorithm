cc92306ab2ed7a2f28835a05c51f993a3fd12d37
Bellroute <kjkun7631@gmail.com>
2020-08-18 14:07:28 +0900

    solve: baekjoon1655 / heap

---
+++ src/beckjoon/BaekJoon1655.java
@@ -0,0 +1,39 @@
+package beckjoon;
+
+import java.util.*;
+
+public class BaekJoon1655 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int N = scanner.nextInt();
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.naturalOrder());
+
+        for (int i = 0; i < N; i++) {
+            int number = scanner.nextInt();
+
+            if (i % 2 == 0) {
+                maxHeap.add(number);
+            } else {
+                minHeap.add(number);
+            }
+
+            if (!maxHeap.isEmpty() && !minHeap.isEmpty()) {
+                if (maxHeap.peek() > minHeap.peek()) {
+                    int temp = maxHeap.poll();
+                    maxHeap.add(minHeap.poll());
+                    minHeap.add(temp);
+                }
+            }
+
+            stringBuilder.append(maxHeap.peek())
+                         .append("\n");
+        }
+
+        System.out.println(stringBuilder.toString());
+    }
+}
