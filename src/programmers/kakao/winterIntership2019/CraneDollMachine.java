8efe17a1c2ee2d1a63fc0e05a28e18a2feba8248
Bellroute <kjkun7631@gmail.com>
2020-08-24 19:48:32 +0900

    solve: kakao winter intern 2019 - 크레인 인형 뽑기 게임

---
+++ src/programmers/kakao/winterIntership2019/CraneDollMachine.java
@@ -0,0 +1,49 @@
+package programmers.kakao.winterIntership2019;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CraneDollMachine {
+
+    private Stack<Integer> bucket = new Stack<>();
+    private int answer = 0;
+
+    @Test
+    public void test() {
+        assertEquals(4, solution(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}}, new int[] {1, 5, 3, 5, 1, 2, 1, 4}));
+    }
+
+    public int solution(int[][] board, int[] moves) {
+        int height = board.length;
+
+        for (int target : moves) {
+            target--;
+
+            for (int j = 0; j < height; j++) {
+                if (board[j][target] != 0) {
+                    pick(board[j][target]);
+                    board[j][target] = 0;
+
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private void pick(int item) {
+        if (!bucket.empty() && bucket.peek() == item) {
+            bucket.pop();
+            answer += 2;
+
+            return;
+        }
+
+        bucket.push(item);
+    }
+}
