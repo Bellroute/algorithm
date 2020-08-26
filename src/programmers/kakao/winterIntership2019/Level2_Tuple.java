caedd16f2692659fa648c593edf95c2da989701d
Bellroute <kjkun7631@gmail.com>
2020-08-26 15:52:00 +0900

    solve: kakao intern 2019 - level2_튜플

---
+++ src/programmers/kakao/winterIntership2019/Level2_Tuple.java
@@ -0,0 +1,55 @@
+package programmers.kakao.winterIntership2019;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class Level2_Tuple {
+
+    public int[] solution(String s) {
+        String parsedInput = s.replace("{", "")
+                              .replace("}", "")
+                              .replace(",", " ");
+
+        String[] numbers = parsedInput.split(" ");
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String number : numbers) {
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+
+        List<CompareObject> list = new ArrayList<>();
+
+        for (String key : map.keySet()) {
+            list.add(new CompareObject(key, map.get(key)));
+        }
+
+        Collections.sort(list);
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = Integer.valueOf(list.get(i).key);
+        }
+
+        return answer;
+    }
+
+    class CompareObject implements Comparable {
+
+        String key;
+        int value;
+
+        public CompareObject(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            CompareObject object = (CompareObject) o;
+
+            return object.value - this.value;
+        }
+    }
+}
