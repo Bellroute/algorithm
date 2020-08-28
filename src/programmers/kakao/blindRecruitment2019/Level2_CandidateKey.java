20cbc281f747c25e82fa0dd8b92d1d4adb66e909
Bellroute <kjkun7631@gmail.com>
2020-08-28 15:56:38 +0900

    solve: kakao blind 2019 - Level2_후보키

--- src/programmers/kakao/blindRecruitment2019/Level2_CandidateKey.java
+++ src/programmers/kakao/blindRecruitment2019/Level2_CandidateKey.java
@@ -2,87 +2,73 @@ package programmers.kakao.blindRecruitment2019;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Level2_CandidateKey {
 
-    private static int numberOfColumn;
-    private static int numberOfRow;
-    private static String[][] table;
-    private static ArrayList<Integer> keyList = new ArrayList<>();
+    private int rowSize;
+    private int colSize;
+    List<Integer> candidates = new ArrayList<>();
+    private String[][] db;
 
-    public static void main(String[] args) {
-        String[][] relation = {
-                {"100", "ryan", "music", "2"},
-                {"200", "apeach", "math", "2"},
-                {"300", "tube", "computer", "3"},
-                {"400", "con", "computer", "4"},
-                {"500", "muzi", "music", "3"},
-                {"600", "apeach", "music", "2"}
-        };
+    public int solution(String[][] relation) {
+        rowSize = relation.length;
+        colSize = relation[0].length;
+        db = relation;
 
-        System.out.println(solution(relation));
-
-//        keyList.forEach(System.out::println);
-    }
-
-    public static int solution(String[][] relation) {
-        numberOfColumn = relation[0].length;
-        numberOfRow = relation.length;
-        table = relation;
-
-        int answer = 0;
-
-        for (int bitMask = 0; bitMask < (1 << numberOfColumn); bitMask++) {
-            if (isSubKey(bitMask)) {
+        for (int i = 0; i < (1 << colSize); i++) {
+            if (!isUnique(i)) {
                 continue;
             }
 
-            if (isUnique(bitMask)) {
-                answer++;
-                keyList.add(bitMask);
+            if (!isMinimum(i)) {
+                continue;
             }
+
+            candidates.add(i);
         }
 
-        return answer;
+        return candidates.size();
     }
 
-    private static boolean isSubKey(int bitMask) {
-        for (Integer subKey : keyList) {
-            if ((bitMask & subKey) >= subKey) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static boolean isUnique(int bitMask) {
-        HashSet<String> checkKeySet = new HashSet<>();
-        StringBuilder stringBuilder = new StringBuilder();
-
-        if (bitMask == 0) return false;
-
-        for (int i = 0; i < numberOfRow; i++) {
-            stringBuilder.setLength(0);
-
-            for (int j = 0; j < numberOfColumn; j++) {
-
-                if ((bitMask & (1 << j)) != 0) {
-                    stringBuilder.append(table[i][j]);
-                }
-            }
-
-            String key = stringBuilder.toString();
-//            System.out.println(key);
-
-            if (!checkKeySet.contains(key)) {
-                checkKeySet.add(key);
-            } else {
+    private boolean isMinimum(int set) {
+        for (int candidateKey : candidates) {
+            if ((set & candidateKey) == candidateKey) { // & 비트 연산은 둘 다 1일 경우에 1을 리턴, set & key == key라면 set은 key를 포함하고 있다는 말이 됨.
                 return false;
             }
         }
 
         return true;
     }
+
+    private boolean isUnique(int set) {
+        Set<String> candidateSet = new HashSet<>();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < colSize; i++) {
+            if (((set >> i) & 1) == 1) {
+                list.add(i);
+            }
+        }
+
+        for (int i = 0; i < rowSize; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int j = 0; j < list.size(); j++) {
+                stringBuilder.append(db[i][list.get(j)]);
+            }
+
+            if (candidateSet.contains(stringBuilder.toString())) {
+                return false;
+            }
+
+            candidateSet.add(stringBuilder.toString());
+        }
+
+        return true;
+    }
+
 
 }
