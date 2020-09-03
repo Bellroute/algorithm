773d8689bbd3ae200fad010034044a5af92335df
Bellroute <kjkun7631@gmail.com>
2020-09-03 11:18:18 +0900

    resolve: kakao blind 2018 - level1_비밀지도

--- src/programmers/kakao/blindRecruitment2018/Level1_SecretMap.java
+++ src/programmers/kakao/blindRecruitment2018/Level1_SecretMap.java
@@ -1,30 +1,67 @@
 package programmers.kakao.blindRecruitment2018;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class Level1_SecretMap {
 
-    public static void main(String[] args) {
-        String[] a = solution(6, new int[]{46, 33, 33 ,22, 31, 50}, new int[]{27 ,56, 19, 14, 14, 10});
+    private static final String WALL = "#";
+    private static final String BLANK = " ";
 
-        for (int i = 0; i < 6; i++) {
-            System.out.println(a[i]);
-        }
+    @Test
+    public void test() {
+        assertEquals(new String[]{"#####", "# # #", "### #", "#  ##", "#####"}, solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28}));
+        assertEquals(new String[]{"######", "###  #", "##  ##", " #### ", " #####", "### # "}, solution(6, new int[]{46, 33, 33, 22, 31, 50}, new int[]{27, 56, 19, 14, 14, 10}));
     }
 
-    public static String[] solution(int n, int[] arr1, int[] arr2) {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
 
         for (int i = 0; i < n; i++) {
-            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            StringBuilder stringBuilder = new StringBuilder();
+            int overlappedElement = (arr1[i] | arr2[i]);
+
+            for (int j = n - 1; j >= 0; j--) {
+                if ((overlappedElement & (1 << j)) != 0) {
+                    stringBuilder.append(WALL);
+                    continue;
+                }
+
+                stringBuilder.append(BLANK);
+            }
+
+            System.out.println(stringBuilder.toString());
+            answer[i] = stringBuilder.toString();
         }
-
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = String.format("%" + n + "s", answer[i]);
-            answer[i] = answer[i].replaceAll("1", "#");
-            answer[i] = answer[i].replaceAll("0", " ");
-        }
-
-
 
         return answer;
     }
+
+//    public static void main(String[] args) {
+//        String[] a = solution(6, new int[]{46, 33, 33 ,22, 31, 50}, new int[]{27 ,56, 19, 14, 14, 10});
+//
+//        for (int i = 0; i < 6; i++) {
+//            System.out.println(a[i]);
+//        }
+//    }
+//
+//    public static String[] solution(int n, int[] arr1, int[] arr2) {
+//        String[] answer = new String[n];
+//
+//        for (int i = 0; i < n; i++) {
+//            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+//        }
+//
+//        for (int i = 0; i < answer.length; i++) {
+//            answer[i] = String.format("%" + n + "s", answer[i]);
+//            answer[i] = answer[i].replaceAll("1", "#");
+//            answer[i] = answer[i].replaceAll("0", " ");
+//        }
+//
+//
+//
+//        return answer;
+//    }
 }
