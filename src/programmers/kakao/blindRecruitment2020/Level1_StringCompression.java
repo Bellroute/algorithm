26c2eea3d1f22def50d717fd01e8a35824f8199e
Bellroute <kjkun7631@gmail.com>
2020-09-04 14:42:43 +0900

    resolve: kakao blind 2020 level1_문자열 압축

--- src/programmers/kakao/blindRecruitment2020/Level1_StringCompression.java
+++ src/programmers/kakao/blindRecruitment2020/Level1_StringCompression.java
@@ -1,17 +1,25 @@
 package programmers.kakao.blindRecruitment2020;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class Level1_StringCompression {
-    public static void main(String[] args) {
-        System.out.println("answer: " + solution("aabbaccc"));
-        System.out.println("answer: " + solution("ababcdcdababcdcd"));
-        System.out.println("answer: " + solution("abcabcdede"));
-        System.out.println("answer: " + solution("abcabcabcabcdededededede"));
-        System.out.println("answer: " + solution("xababcdcdababcdcd"));
+
+    @Test
+    public void test() {
+        assertEquals(7, solution("aabbaccc"));
+        assertEquals(9, solution("ababcdcdababcdcd"));
+        assertEquals(8, solution("abcabcdede"));
+        assertEquals(14, solution("abcabcabcabcdededededede"));
+        assertEquals(17, solution("xababcdcdababcdcd"));
+        assertEquals(1, solution("a"));
     }
 
-    public static int solution(String s) {
-        int answer = s.length();
-        int tries = s.length() / 2;
+    public int solution(String s) {
+        int answer = Integer.MAX_VALUE;
+        int length = s.length();
+        int tries = length / 2;
 
         if (s.length() == 1) {
             return 1;
@@ -19,21 +27,19 @@ public class Level1_StringCompression {
 
         for (int i = 1; i <= tries; i++) {
             String result = compress(s, i);
-            if (answer > result.length()) {
-                answer = result.length();
-            }
+            answer = Math.min(answer, result.length());
         }
 
         return answer;
     }
 
-    private static String compress(String s, int unit) {
+    private String compress(String s, int unit) {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < s.length(); i += unit) {
             int count = 1;
 
-            if (i + unit > s.length() ) {
+            if (i + unit > s.length()) {
                 result.append(s.substring(i));
                 break;
             }
@@ -41,7 +47,7 @@ public class Level1_StringCompression {
             String prev = s.substring(i, i + unit);
 
             for (int j = i + unit; j < s.length(); j += unit) {
-                if (j + unit > s.length() ) {
+                if (j + unit > s.length()) {
                     break;
                 }
 
@@ -64,6 +70,74 @@ public class Level1_StringCompression {
             result.append(prev);
         }
 
+
         return result.toString();
     }
+
+
+//    public static void main(String[] args) {
+//        System.out.println("answer: " + solution("aabbaccc"));
+//        System.out.println("answer: " + solution("ababcdcdababcdcd"));
+//        System.out.println("answer: " + solution("abcabcdede"));
+//        System.out.println("answer: " + solution("abcabcabcabcdededededede"));
+//        System.out.println("answer: " + solution("xababcdcdababcdcd"));
+//    }
+//
+//    public static int solution(String s) {
+//        int answer = s.length();
+//        int tries = s.length() / 2;
+//
+//        if (s.length() == 1) {
+//            return 1;
+//        }
+//
+//        for (int i = 1; i <= tries; i++) {
+//            String result = compress(s, i);
+//            if (answer > result.length()) {
+//                answer = result.length();
+//            }
+//        }
+//
+//        return answer;
+//    }
+//
+//    private static String compress(String s, int unit) {
+//        StringBuilder result = new StringBuilder();
+//
+//        for (int i = 0; i < s.length(); i += unit) {
+//            int count = 1;
+//
+//            if (i + unit > s.length() ) {
+//                result.append(s.substring(i));
+//                break;
+//            }
+//
+//            String prev = s.substring(i, i + unit);
+//
+//            for (int j = i + unit; j < s.length(); j += unit) {
+//                if (j + unit > s.length() ) {
+//                    break;
+//                }
+//
+//                String post = s.substring(j, j + unit);
+//
+//                if (prev.equals(post)) {
+//                    count++;
+//                    i += unit;
+//                } else {
+//                    break;
+//                }
+//            }
+//
+//            if (count == 1) {
+//                result.append(prev);
+//                continue;
+//            }
+//
+//            result.append(count);
+//            result.append(prev);
+//        }
+//
+//        return result.toString();
+//    }
 }
