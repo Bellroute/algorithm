9c8f5520660215250c784c148613cdaaa73e73c4
Bellroute <kjkun7631@gmail.com>
2021-01-22 18:20:49 +0900

    solve: 파이썬 알고리즘 인터뷰 ch9_22

---
+++ python/book_python_algorithm_interview/ch9/daily_temperatures.py
@@ -0,0 +1,17 @@
+# 리트코드 739. Daily Temperatures
+
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        stack = []
+        answer = [0] * len(T)
+
+        for i, cur in enumerate(T):
+            while stack and cur > T[stack[-1]]:
+                last = stack.pop()
+                answer[last] = i - last
+            stack.append(i)
+
+        return answer
